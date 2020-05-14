508f54e3115c6eae57942cd924392be2228acae5
MirMiao <1251267855@qq.com>
2020-05-14 08:28:14 +0800

    影音项目

---
+++ app/src/main/java/com/wd/videodemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wd.videodemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
